a9b80206d6c5727c69b6fa5ca7d8a3415afebdf8
vancomm <vanushnick@gmail.com>
2022-03-14 00:23:47 +0300

    Tweaked output style

--- src/question/table.ts
+++ src/question/table.ts
@@ -2,18 +2,38 @@ import { CheckboxQuestion } from 'inquirer';
 import { Item } from '../types.js';
 import { makeCheckboxQuestion } from './utils.js';
 
+const NAME_LENGTH = 32;
+
+function splitName(name: string): [string, string | undefined] {
+  if (name.length < NAME_LENGTH) return [name, undefined];
+  const spaceIndex = name.slice(0, NAME_LENGTH - 1).lastIndexOf(' ');
+  const first = name.slice(0, spaceIndex);
+  const second = name.slice(spaceIndex);
+  return [first, second];
+}
+
 export default function table(item: Item, choices: string[]): CheckboxQuestion {
   const { name, quantity } = item;
-  const price = item.price / 100;
-  const moreThanOne = (quantity === 1);
-  const quantityText = moreThanOne
-    ? ''
-    : `\t${quantity}`;
-  const sumText = moreThanOne
-    ? ''
-    : `\t${item.sum / 100} RUB`;
 
-  const message = `\t${name}\t${price} RUB${quantityText}${sumText}\n`;
+  const [name1, name2] = splitName(name);
+  const nameTextTop = `\t${name1.padEnd(NAME_LENGTH, ' ')}`;
+  const nameTextBtm = name2
+    ? `\n\t${name2.trimStart()}`
+    : '';
+
+  const price = `${(item.price / 100).toFixed(2)} RUB`;
+
+  const moreThanOne = (quantity > 1);
+
+  const quantityText = moreThanOne
+    ? `\t${quantity}`
+    : '';
+
+  const sumText = moreThanOne
+    ? `\t${item.sum / 100} RUB`
+    : '';
+
+  const message = `${nameTextTop}${price}${quantityText}${sumText}${nameTextBtm}\n`;
 
   return makeCheckboxQuestion(name, message, choices);
 }
